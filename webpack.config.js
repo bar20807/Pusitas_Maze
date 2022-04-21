8b4fe340f1d762f30f3a46b626dde9354fef42b8
bar20807@uvg.edu.gt <Lolo1234>
2022-04-20 23:35:18 -0600

    Música agregada

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const webpack = require('webpack');
 
@@ -13,11 +11,7 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Maze'
-        })
-    ],
+
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
